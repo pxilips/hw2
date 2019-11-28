746620e00e0efd9e7944e187a5f551e1d8421bb2
pxilips <58148159+pxilips@users.noreply.github.com>
2019-11-28 21:37:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,26 +5,41 @@ data "aws_ami" "centos" {
     name   = "name"
     values = ["CentOS Linux 7 x86_64 HVM EBS *"]
   }
-
-  filter {
+	filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
   owners = ["679593333241"]
 }
+
+
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.centos.id}"
   instance_type = "t2.micro"
   key_name = "centos_aws_ssh"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  root_block_device {
+  delete_on_termination = true
+  }
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
   tags = {
     Name = "HelloWorld"
-  }
+}
+connection {
+	type     = "ssh"
+	user     = "centos"
+	private_key = file("centos_aws_ssh.pem")
+	host = "${self.public_ip}"
+      }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y"
+	  ]
+	}
 }
 
 
 resource "aws_security_group" "allow_ssh" {
-name = "allow_ssh"
+name = "allow ssh"
 description = "Security group for server HelloWorld"
 ingress { 
 from_port = 22 
